1657e5462cc3b10be183f927c40a720eff03fac1
Paraliyzed_evo <107338855+ParaliyzedEvo@users.noreply.github.com>
2025-07-30 13:28:53 -0500

    Update background.js

--- background.js
+++ background.js
@@ -3,15 +3,12 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     fetch(message.url, {
       method: message.method || "GET",
       headers: message.headers || {},
-      body: message.body || null
+      body: message.body || null,
     })
-      .then(async (res) => {
-        const text = await res.text();
-        sendResponse({ success: true, data: text });
-      })
-      .catch((err) => {
-        sendResponse({ success: false, error: err.toString() });
-      });
+      .then(res => res.text())
+      .then(data => sendResponse({ success: true, data }))
+      .catch(err => sendResponse({ success: false, error: err.toString() }));
+
     return true;
   }
 });
