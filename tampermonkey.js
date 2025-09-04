56af36b99e6d1ac0b630c7838118d99276b66e3b
Paraliyzed_evo <107338855+ParaliyzedEvo@users.noreply.github.com>
2025-09-03 19:33:46 -0500

    Update version

--- tampermonkey.js
+++ tampermonkey.js
@@ -7,7 +7,7 @@
 // @downloadURL  https://github.com/ParaliyzedEvo/osu-Challengers-Extension/releases/latest/download/tampermoney.js
 // @author       Paraliyzed_evo and Thunderbirdo
 // @icon         https://osu.ppy.sh/favicon.ico
-// @version      2.2.2
+// @version      2.2.3
 // @description  Extension to view osu!Challenger stats on osu!Website.
 // @match        https://osu.ppy.sh/*
 // @connect      ppy.sh
@@ -22,7 +22,7 @@
 	async function runScript() {
 	  if (!/^https:\/\/osu\.ppy\.sh\/(users|u)\/\d+/.test(location.href)) return;
 	  await new Promise(res => requestAnimationFrame(res));
-	  console.log('[OTC] 🔥 v2.2.2 start');
+	  console.log('[OTC] 🔥 v2.2.3 start');
 
 	  const osuId = parseInt(location.pathname.split('/')[2], 10);
 	  if (!osuId) return;
