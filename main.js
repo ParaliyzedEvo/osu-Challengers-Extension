99b99e50e268b81fff967c35367308ccdd3a04dd
Paraliyzed_evo <107338855+ParaliyzedEvo@users.noreply.github.com>
2025-11-12 20:32:47 -0600

    Change version

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 	async function runScript() {
 	  if (!/^https:\/\/osu\.ppy\.sh\/(users|u)\/\d+/.test(location.href)) return;
 	  await new Promise(res => requestAnimationFrame(res));
-	  console.log('[OTC] 🔥 v2.4.7 start');
+	  console.log('[OTC] 🔥 v2.4.8 start');
 
 	  const osuId = parseInt(location.pathname.split('/')[2], 10);
 	  if (!osuId) return;
